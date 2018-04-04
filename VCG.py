2c1663a7889cacd8fc09b93243ade618d7e69cd5
Aaron <aaron.morgenegg@verizondigitalmedia.com>
2018-04-03 19:34:03 -0600

    Working on cost adjustment for vickrey

--- VCG.py
+++ VCG.py
@@ -15,34 +15,51 @@ def getVickreySlots(slots, bidders):
     # Assign slots to highest bidders
     for i in range(len(bidders)):
         slot = i
+
+        try:
+            cost = slots[i]
+        except IndexError:
+            cost = 0
         if slot >= len(slots):
             slot = None
-        VCG.append({'slot': slot})
+        VCG.append({'slot': slot, 'cost': cost, 'bidder': bidders[i]})
 
-    VCG = getVickreyCosts(slots, bidders, VCG)
     return VCG
 
 def getVickreyCosts(slots, bidders, VCG):
+    VCG_copy = list(VCG)
     for i in range(len(bidders)):
-        VCG[i]['cost'] = getVickreyCost(slots, bidders, VCG, i)
-    return VCG
+        getVickreyCost(slots, bidders, VCG_copy, i)
+    return VCG_copy
 
 def getVickreyCost(slots, bidders, VCG, i):
-    try:
-        cost = slots[VCG[i]['slot']]
-    except TypeError:
-        return 0
     bidders_copy = list(bidders)
     bidders_copy.remove(bidders[i])
     VCG2 = getVickreySlots(slots, bidders_copy)
     VCG_copy = list(VCG)
-    print(VCG2)
-    print(VCG)
+    VCG[i]['cost'] -= getVickreyCostAdjustment(VCG2, VCG_copy, i)
 
-    return 0
+def getVickreyCostAdjustment(VCG2, VCG, i):
+    cost_adjustment = 0
+    for j in range(len(VCG2)):
+        ranking = VCG2[j]
+        if ranking['bidder']['bidder'] > i:
+            cost_adjustment += (ranking['cost'] - findBidder(ranking['bidder']['bidder'], VCG)['cost'])
+    return cost_adjustment
+
+def findBidder(i, VCG):
+    for ranking in VCG:
+        if ranking['bidder']['bidder'] == i:
+            return ranking
+    raise Exception
 
 if __name__ == '__main__':
     slots = [500, 300, 100]
-    bidders = [.5, .4, .3, .2, .1]
+    bidders = [{'bid':.5, 'bidder': 0},
+               {'bid': .4, 'bidder': 1},
+               {'bid': .3, 'bidder': 2},
+               {'bid': .2, 'bidder': 3},
+               {'bid': .1, 'bidder': 4}]
+
     VCG = vickreyClarkGrove(slots, bidders)
     print(VCG)
